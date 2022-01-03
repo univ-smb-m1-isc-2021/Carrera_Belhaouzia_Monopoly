6c02c7854dccad02d40500b10994094e80192ac2
TheVatsu <66404934+TheVatsu@users.noreply.github.com>
2022-01-03 17:56:15 +0100

    junit

---
+++ src/test/JUnitTest.java
@@ -0,0 +1,18 @@
+package test;
+
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+
+
+class JUnitTest extends TestCase{
+
+	@Test
+	void test() {
+	    System.out.println("simpleTest");
+	    assertTrue(true);
+	}
+
+}
