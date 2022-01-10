b2bb3ca1968c2dd199cffd51d30a0a437649371a
TheVatsu <66404934+TheVatsu@users.noreply.github.com>
2022-01-10 16:03:07 +0100

    jk

--- src/test/java/TestPlateau.java
+++ src/test/java/TestPlateau.java
@@ -16,6 +16,7 @@ public class TestPlateau extends TestCase{
 	    assertTrue(p.passerdepart(34, 0) == true);
 	    assertTrue(p.passerdepart(12,24) == false);
 	    assertTrue(p.passerdepart(23, 12) == true);
+	    assertTrue(p.passerdepart(0, 12) == false);
 	    
 	}
 }
