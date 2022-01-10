72da05829e1abf3420988075d88cddb218c6ae1f
TheVatsu <66404934+TheVatsu@users.noreply.github.com>
2022-01-10 15:37:25 +0100

    push pou zoz

---
+++ src/test/java/TestJoueur.java
@@ -0,0 +1,27 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+import main.java.CasePropriete;
+import main.java.Joueur;
+
+public class TestJoueur extends TestCase{
+	public TestJoueur(String name) {
+		super(name);
+	}
+	@Test
+	public void test() {
+		Joueur j = new Joueur("Zohir");
+	    System.out.println("Test joueur : ");
+	    j.debiterJoueur(500);
+	    assertTrue(j.argent == 1000);
+	    j.crediterJoueur(500);
+	    assertTrue(j.argent == 1500);
+	    int size = j.l.size();
+	    j.ajouterCase(new CasePropriete("Boulevard de belleville",60, 100,null));
+	    assertTrue(j.l.size() == size + 1);
+	    assertTrue(j.l.get(0).nom.equals("Boulevard de belleville"));
+	}
+
+}
