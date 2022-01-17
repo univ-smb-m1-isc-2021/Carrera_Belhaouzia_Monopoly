6bffb1bdaa07ef67575a68f96cfcc6483d5626f0
TheVatsu <66404934+TheVatsu@users.noreply.github.com>
2022-01-17 16:21:37 +0100

    test coverage

---
+++ TestMain.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+import test.java.CasePropriete;
+import test.java.Joueur;
+import test.java.Quartier;
+
+public class TestMain extends TestCase{
+
+	
+	@Test
+	public void test() {
+		Main m = new Main();
+	}
+}
